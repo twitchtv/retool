d42731d225cc305f4a0bb8d8cfca9e0cf80d4c76
Spencer Nelson <spencer@justin.tv>
2016-11-18 17:03:01 -0500

    Resolve symlinks when sweeping out unused files in the clean step
    Summary:
    If a package vendors its code with godep and keeps a symlink pointing to Godeps/_workspace/src from vendor, then we should keep any files we need inside the Godeps directory.
    
    More generally, we should obey symlinks. So, this revision evaluates symlinks in package paths before walking through and discarding unused folders/files.
    
    Test Plan: Ran against code.justin.tv/timotyen/testrepo
    
    Reviewers: timotyen
    
    Reviewed By: timotyen
    
    Subscribers: kelleyk
    
    Differential Revision: https://phabricator.internal.justin.tv/D4608

--- clean.go
+++ clean.go
@@ -14,7 +14,7 @@ func clean(pkgs []string) {
 	build.Default.GOPATH = toolDirPath
 
 	// A recursive helper to take a list of packages and find their dependencies deeply
-	found := map[string]struct{}{}
+	keep := map[string]struct{}{}
 	var resolve func(string, []string) []string
 	resolve = func(parent string, pkgs []string) []string {
 		var r []string
@@ -28,19 +28,37 @@ func clean(pkgs []string) {
 				fatal(fmt.Sprintf("couldn't import package %q", pkg), err)
 			}
 
-			if _, ok := found[p.ImportPath]; ok {
+			if _, ok := keep[p.ImportPath]; ok {
 				continue
 			}
 
-			found[p.ImportPath] = struct{}{}
+			keep[p.ImportPath] = struct{}{}
 			r = append(r, p.ImportPath)
 			r = append(r, resolve(p.ImportPath, p.Imports)...)
 		}
 		return r
 	}
 
-	keep := resolve("", pkgs)
+	resolve("", pkgs)
+
 	base := path.Join(toolDirPath, "src")
+
+	// Resolve any symlinks in the packages to keep, because we're going
+	// to walk through the file system, so we need to trim stuff by
+	// _filename_.
+	for pkgPath := range keep {
+		fullPath := filepath.Join(base, pkgPath)
+		resolved, err := filepath.EvalSymlinks(fullPath)
+		if err != nil {
+			fatal(fmt.Sprintf("couldn't eval symlinks in %q", pkgPath), err)
+		}
+		// Undo the filepath.Join from above
+		pkgPath, err = filepath.Rel(base, resolved)
+		if err != nil {
+			fatal(fmt.Sprintf("couldn't eval symlinks in %q", pkgPath), err)
+		}
+		keep[pkgPath] = struct{}{}
+	}
 
 	var toDelete []string
 	err := filepath.Walk(base, func(path string, info os.FileInfo, err error) error {
@@ -62,9 +80,9 @@ func clean(pkgs []string) {
 
 		// Delete files in packages that aren't marked as "keep",
 		// and any non-go or test files.
-		if !info.IsDir() {
+		if info.Mode().IsRegular() {
 			pkg = filepath.Dir(pkg)
-			_, keptPkg := found[pkg]
+			_, keptPkg := keep[pkg]
 			isGo := strings.HasSuffix(path, ".go")
 			isAssembly := strings.HasSuffix(path, ".s")
 			isTest := strings.HasSuffix(path, "_test.go")
@@ -75,7 +93,7 @@ func clean(pkgs []string) {
 		}
 
 		// If the folder is a kept package or a parent, don't delete it and keep recursing
-		for _, p := range keep {
+		for p := range keep {
 			if strings.HasPrefix(p, pkg) {
 				return nil
 			}
