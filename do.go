b5d570cee0ef983f26940a0109c5471d0623966a
Spencer Nelson <spencer@twitch.tv>
2017-03-23 10:09:24 -0400

    Set GOPATH and GOBIN in retool do
    This is required for gometalinter support, and makes sense to do anyway in
    general. This convinces tools to use the vendored _tools directory.

--- do.go
+++ do.go
@@ -16,13 +16,40 @@ func setPath() (unset func()) {
 	}
 }
 
+func setGoEnv() (unset func()) {
+	prevGoPath, goPathWasSet := os.LookupEnv("GOPATH")
+	newGoPath := toolDirPath
+	_ = os.Setenv("GOPATH", newGoPath)
+
+	prevGoBin, goBinWasSet := os.LookupEnv("GOBIN")
+	newGoBin := filepath.Join(toolDirPath, "bin")
+	_ = os.Setenv("GOBIN", newGoBin)
+
+	return func() {
+		if goPathWasSet {
+			_ = os.Setenv("GOPATH", prevGoPath)
+		} else {
+			_ = os.Unsetenv("GOPATH")
+		}
+		if goBinWasSet {
+			_ = os.Setenv("GOBIN", prevGoBin)
+		} else {
+			_ = os.Unsetenv("GOBIN")
+		}
+	}
+}
+
 func do() {
 	args := positionalArgs
 	if len(args) == 0 {
 		fatal("no command passed to retool do", nil)
 	}
 
-	defer setPath()()
+	unsetPath := setPath()
+	defer unsetPath()
+
+	unsetGoEnv := setGoEnv()
+	defer unsetGoEnv()
 
 	cmd := exec.Command(args[0], args[1:]...)
 	cmd.Stdin = os.Stdin
